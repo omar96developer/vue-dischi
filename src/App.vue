c4f59a4a12bf5f1622816016b3f89521f24f9186
omar96developer <omakino@hotmail.it>
2021-11-15 17:10:39 +0100

    0.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+      <div class="container">
+        <Songcontainer></Songcontainer>
+
+      </div>
+    </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Songcontainer from './components/Songcontainer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Songcontainer
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "@/style/app";
+
 </style>
---
+++ src/components/Songcontainer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>hjbsdiub</h1>
+        <div v-for="(song, i) in songList" :key="i">
+            {{song.title}}
+        </div>
+    </div>
+    
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: 'Songcontainer',
+    data() {
+        return {
+            songList: '',
+        }
+
+    },
+    mounted() {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then(resp => {
+            this.songList = resp.data.response
+            console.log(resp)
+        })
+    }
+}
+</script>
